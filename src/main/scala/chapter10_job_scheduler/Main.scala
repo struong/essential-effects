1a32fcaa6b0f7f27c5c251af4b1e5bc6e62048a6
struong <45595406+struong@users.noreply.github.com>
2022-04-19 23:22:56 +0100

    Fixed await

--- src/main/scala/chapter10_job_scheduler/Main.scala
+++ src/main/scala/chapter10_job_scheduler/Main.scala
@@ -8,21 +8,16 @@ import scala.concurrent.duration._
 
 object Main extends IOApp {
 
-  override def run(args: List[String]): IO[ExitCode] = {
-    def createTask(i: Int): IO[Unit] =
-      IO.sleep((200 + i * 10).millisecond) *>
-        IO(s"Task $i").debug.void
+  def createTask(i: Int): IO[Unit] =
+    IO.sleep((200 + i * 100).millisecond) *>
+      IO(s"Task $i").debug.void
 
-    for {
-      resource <- JobScheduler.resource(2)
-      _ <- resource.use { scheduler =>
-        (1 to 50)
-          .map(createTask)
-          .toList
-          .traverse(scheduler.schedule(_)) *>
-          IO.sleep(1.minute)
-      }
-      _ <- IO("Finished!").debug
-    } yield ExitCode.Success
+  val numTasks = 100
+  val tasks: List[Int] = List.range(0, numTasks)
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    tasks
+      .traverse(createTask) // could use parTraverse?
+      .as(ExitCode.Success)
   }
 }
