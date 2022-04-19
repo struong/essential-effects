574da1a0292668f80b82ca0f095201d39afaed16
struong <45595406+struong@users.noreply.github.com>
2022-04-19 23:26:13 +0100

    Use parTraverse

--- src/main/scala/chapter10_job_scheduler/Main.scala
+++ src/main/scala/chapter10_job_scheduler/Main.scala
@@ -8,16 +8,21 @@ import scala.concurrent.duration._
 
 object Main extends IOApp {
 
-  def createTask(i: Int): IO[Unit] =
-    IO.sleep((200 + i * 100).millisecond) *>
-      IO(s"Task $i").debug.void
-
-  val numTasks = 100
-  val tasks: List[Int] = List.range(0, numTasks)
-
   override def run(args: List[String]): IO[ExitCode] = {
-    tasks
-      .traverse(createTask) // could use parTraverse?
-      .as(ExitCode.Success)
+    def createTask(i: Int): IO[Unit] =
+      IO.sleep((200 + i * 10).millisecond) *>
+        IO(s"Task $i").debug.void
+
+    for {
+      resource <- JobScheduler.resource(2)
+      _ <- resource.use { scheduler =>
+        (1 to 50)
+          .map(createTask)
+          .toList
+          .parTraverse(scheduler.schedule(_)) *>
+          IO.sleep(1.minute)
+      }
+      _ <- IO("Finished!").debug
+    } yield ExitCode.Success
   }
 }
